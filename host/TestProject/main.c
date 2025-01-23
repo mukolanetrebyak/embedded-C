8b8c30961d8b56bcd18700a8302fcd88b2629bcc
mukolanetrebyak <mukolanetr1@gmail.com>
2025-01-24 01:19:39 +0200

    add some files

---
+++ host/TestProject/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("David says , \" Programming is fun !\"\n");
+	printf("**Conditionals , \"Offer valid until tommorow\"\n");
+	printf("C:\\My computer\\My folder\n");
+	printf("D:/My Documents/My folder\n");
+	printf("\\\\\\\\ Today id holiday\n");
+	printf("This is a triple string \"\"\"This month has 30 days\"\"\"");
+	getchar();
+	return 0;
+}
