5e8a1fea768e9b28b6729db6f206056b92774464
mukolanetrebyak <mukolanetr1@gmail.com>
2025-01-26 21:41:57 +0200

    add some files 1.2

---
+++ host/other/dest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned char destA2B = 10;
+    unsigned char destB2C = 17;
+    unsigned char destC2A;
+
+    destC2A = destA2B + destB2C;
+    //printf("Відстань між пунктами A і С = %u\n", destC2A);
+    printf("Величина = %d\n", sizeof(long long));
+}
